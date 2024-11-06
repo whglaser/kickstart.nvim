3df4d40bc4d3784a154a706923bb9030020d1fea
William Glaser <william.glaser@stewart.com>
2024-11-05 20:23:42 -0600

    Added Copilot.vim, thanks tpope!

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -32,4 +32,7 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   },
+  {
+    'github/copilot.vim',
+  },
 }
